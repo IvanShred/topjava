975711dbefe3229d343144d06a2cb4f56d0e34ee
HammerFall <hammerfall-91@mail.ru>
2019-03-09 10:47:55 +0700

    HW05 - context

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.web;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.util.StringUtils;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.web.meal.MealRestController;
@@ -23,17 +24,18 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 public class MealServlet extends HttpServlet {
 
-    private ConfigurableApplicationContext springContext;
+    private ConfigurableApplicationContext /*GenericXmlApplicationContext*/ springContext;
     private MealRestController mealController;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         System.setProperty("spring.profiles.active", "jpa, postgres");
-//        springContext = new ClassPathXmlApplicationContext();
-//        springContext.getEnvironment().addActiveProfile("jpa, postgres");
-//        springContext.refresh();
         springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+//        springContext = new GenericXmlApplicationContext();
+//        springContext.getEnvironment().setActiveProfiles("jpa, postgres");
+//        springContext.load("spring/spring-app.xml", "spring/spring-db.xml");
+//        springContext.refresh();
         mealController = springContext.getBean(MealRestController.class);
     }
 
