fa6b52b70cf6baeb04928c15021f6823538911fb
HammerFall <hammerfall-91@mail.ru>
2019-04-14 00:41:08 +0700

    HW09 - changed setter calories

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -93,7 +93,7 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
     }
 
-    public void setCalories(int calories) {
+    public void setCalories(Integer calories) {
         this.calories = calories;
     }
 
